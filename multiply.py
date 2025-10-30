9a27c2acb72584719558f3034ab93eef56ae6e8a
vivekprasadkyndryl-netizen <vivekprasadkyndryl@gmail.com>
2025-10-30 11:06:18 +0530

    multiplication file

---
+++ multiply.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+def multiply(num1, num2):
+    return num1 * num2
+
+print("The product is:", multiply(num1, num2))
